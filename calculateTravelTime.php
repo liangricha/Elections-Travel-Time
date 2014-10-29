a29eb6cb4676498075dd85a6e472d40616d5e79b
rliang <rliang@etsy.com>
2014-10-29 14:09:35 -0400

    done

--- calculateTravelTime.php
+++ calculateTravelTime.php
@@ -12,19 +12,20 @@ $addresses = preg_split('/$\R?^/m', $address_file);
 
 date_default_timezone_set('America/New_York');
 // We choose 9:00am to give us some time to ger to SEPTA.
-$unix_timestamp = mktime(9, 0, 0, 11, 4, 2014);
+$unix_timestamp = mktime(8, 55, 0, 11, 4, 2014);
 
 $travel_info = array();
 
 // API Params.
 foreach ($addresses as $address) {
+    echo $address , "\n";
     $params = array(
         'origin'         => '500 College Avenue, Swarthmore, PA 19081',
         'destination'    => $address,
         'mode'           => 'transit',
         'departure_time' => $unix_timestamp,
-        'sensor'         => 'true',
-        //'key'            => 'AIzaSyC0w2eSvlh3eeLnj3c0c8XVPW6U_IYfYY4'
+        'alternatives'   => 'true',
+        'key'            => 'AIzaSyC0w2eSvlh3eeLnj3c0c8XVPW6U_IYfYY4'
     );
         
     // Join parameters into URL string
@@ -44,9 +45,28 @@ foreach ($addresses as $address) {
     curl_close($curl);
 
     $directions = json_decode($response);
-    $full_dest_address = $directions->routes[0]->legs[0]->end_address;
-    $travel_time_in_minutes = intval($directions->routes[0]->legs[0]->duration->value) / 60;
+    $route_chosen = NULL;
+    foreach ($directions->routes as $route) {
+        $leg = $route->legs[0];
+        
+        //We prefer transit directions where we ride the 9:06am Septa.
+        if (isset($leg->steps[1]->transit_details->departure_time) && $leg->steps[1]->transit_details->departure_time->text == '9:06am') {
+            if ($route_chosen == NULL) {
+                $route_chosen = $route;
+            } else if ($leg->duration->value < $route_chosen->legs[0]->duration->value) {
+                $route_chosen = $route;
+            }
+        }
+    }
+    
+    //IF we don't encounter traveling directions where we ride the 9:06 Septa, simply choose the first, best, route.
+    if ($route_chosen == NULL) {
+        $route_chosen = $directions->routes[0];
+    }
 
+    $full_dest_address = $route_chosen->legs[0]->end_address;
+    $travel_time_in_minutes = round(intval($route_chosen->legs[0]->duration->value) / 60);
+    
     $info_for_address = $full_dest_address . ' : ' . $travel_time_in_minutes . ' mins';
     array_push($travel_info, $info_for_address);
     // We are limited to 2 requests per second, so we halt for half a second before sending another request.
--- cleanCSV.py
+++ cleanCSV.py
@@ -3,11 +3,16 @@ lines = to_format.read().split('\n')
 lines.pop(0)
 to_format.close()
 
-clean_file = open('addresses.csv', 'w')
+addresses = []
+clean_file = open('addresses.data', 'w')
 for line in lines:
+    if len(line) == 0:
+        continue
     line_contents = line.split(',')
-    address = line_contents[2].lower().title()
+    address = ' '.join([word.capitalize() for word in line_contents[2].split()])
     address = address + ', Philadelphia, PA'
-    clean_file.write(address + '\n')
+    addresses.append(address)
 
+addresses = list(set(addresses))
+clean_file.write('\n'.join(addresses))
 clean_file.close()
